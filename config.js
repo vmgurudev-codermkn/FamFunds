2471005f6d8bda55367ecec610e43a1822217b17
vmgurudev-codermkn <vmgurudev@gmail.com>
2026-07-03 10:09:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // After you deploy the Apps Script as a Web App, paste its URL below.
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
 // ------------------------------------------------------------------
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzDFlY6jsBvimvRxL-3eeYNQMNte30Sut2M85Y7oguaiYY91BjQtxEwSnHtzXggld3Hw/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzDFlY6jsBvimvRxL-K3eeYNQMNte30Sut2M85Y7oguaiYY91BjQtxEwSnHtzXggld3Hw/exec";
